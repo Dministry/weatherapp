4a26a2a2b9a92d674cbdc278c25ea988832dff45
Dministry <dministry356@gmail.com>
2020-07-03 01:15:43 +0100

    updated placeholder

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
         <input 
         type="text"
         class="search-bar" 
-        placeholder="Search..."
+        placeholder="Search for location..."
         v-model="query"
         @keypress="fetchWeather">
       </div>
